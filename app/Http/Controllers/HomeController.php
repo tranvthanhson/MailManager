43e6a854c6c675fd83ee6daf41439162989697b6
Hoai Thuong <thuong.hth@neo-lab.vn>
2017-11-07 13:30:16 +0700

    + add updateEmail funcion in homeController.php and Doned

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -63,43 +63,43 @@ class HomeController extends Controller
                 ->get();
             foreach ($getEmails as $getEmail):
                 echo '<tr style="display: block;">
-			            <td style="width: 50%; display: block; float:left;">' . $getEmail->mail . '@' . $getEmail->extension_content . '</td>
-			            <td style="width: 50%; display: block; float:left; text-align: right;">
-			               <a data-toggle="modal" data-target="#editEmail' . $getEmail->mail_id . '" href="" class="btn btn-success btn-sm" title="Sửa">
-			                   <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
-			               </a>
-			               <!-- Modal -->
-			               <div class="modal fade" id="editEmail' . $getEmail->mail_id . '" role="dialog">
-			                   <div class="modal-dialog">
+						            <td style="width: 50%; display: block; float:left;">' . $getEmail->mail . '@' . $getEmail->extension_content . '</td>
+						            <td style="width: 50%; display: block; float:left; text-align: right;">
+						               <a data-toggle="modal" data-target="#editEmail' . $getEmail->mail_id . '" href="" class="btn btn-success btn-sm" title="Sửa">
+						                   <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+						               </a>
+						               <!-- Modal -->
+						               <div class="modal fade" id="editEmail' . $getEmail->mail_id . '" role="dialog">
+						                   <div class="modal-dialog">
 
-			                       <!-- Modal content-->
-			                       <div class="modal-content">
-			                           <div class="modal-header">
-			                               <button type="button" class="close" data-dismiss="modal">&times;</button>
-			                               <h4 class="modal-title">UPDATE EMAIL</h4>
-			                           </div>
-			                           <form action="/updateEmail/' . $getEmail->mail_id . '" method="POST" >
-			                           <div class="modal-body">
-			                               <div class="form-group">
-			                                   <input type="text" class="form-control" value="' . $getEmail->mail . '" id="" placeholder="Enter mail">
-			                               </div>
-			                               <div class="form-group">
-			                                   <input type="text" value="' . $getEmail->extension_content . '" class="form-control" id="" placeholder="Enter extension">
-			                               </div>
-			                           </div>
-			                           <div class="modal-footer">
-			                               <input type="submit" class="btn btn-primary"  value="Update" />
-			                           </div>
-			                            </form>
-			                       </div>
-			                   </div>
-			               </div>
-			               <a href="/deleteEmail/' . $getEmail->mail_id . '" class="btn btn-danger btn-sm" title="Xoá">
-			                   <i class="fa fa-trash" aria-hidden="true"></i>
-			               </a>
-			               <div class="clearfix"></div>
-			           </td>
-			       </tr>';
+						                       <!-- Modal content-->
+						                       <div class="modal-content">
+						                           <div class="modal-header">
+						                               <button type="button" class="close" data-dismiss="modal">&times;</button>
+						                               <h4 class="modal-title">UPDATE EMAIL</h4>
+						                           </div>
+						                           <form action="/updateEmail/' . $getEmail->mail_id . '" method="POST" >
+						                           <div class="modal-body">
+						                               <div class="form-group">
+						                                   <input type="text" class="form-control" value="' . $getEmail->mail . '" id="" placeholder="Enter mail">
+						                               </div>
+						                               <div class="form-group">
+						                                   <input type="text" value="' . $getEmail->extension_content . '" class="form-control" id="" placeholder="Enter extension">
+						                               </div>
+						                           </div>
+						                           <div class="modal-footer">
+						                               <input type="submit" class="btn btn-primary"  value="Update" />
+						                           </div>
+						                            </form>
+						                       </div>
+						                   </div>
+						               </div>
+						               <a href="/deleteEmail/' . $getEmail->mail_id . '" class="btn btn-danger btn-sm" title="Xoá">
+						                   <i class="fa fa-trash" aria-hidden="true"></i>
+						               </a>
+						               <div class="clearfix"></div>
+						           </td>
+						       </tr>';
             endforeach;
         }
     }
@@ -108,27 +108,42 @@ class HomeController extends Controller
     {
         $mail = $request->mail;
         $extension_content = $request->extension_content;
-        //get Email updating
-        $getEmailUpdate = DB::table('mails')
+        $checkNewEmailAlready = DB::table('mails')
             ->join('extensions', 'extensions.extension_id', '=', 'mails.extension_id')
             ->where([
-                ['mails.mail_id', '=', $id],
+                ['extensions.extension_content', '=', $extension_content],
+                ['mails.mail', '=', $mail],
             ])->get();
-        // dd($getEmailUpdate[0]->mail_id);
-        if ($getEmailUpdate[0]->mail != $mail) {
-            DB::table('mails')
-                ->where('mail_id', $id)
-                ->update(['mail' => $mail]);
-        }
-        if ($getEmailUpdate[0]->extension_content != $extension_content) {
-            //check new extension_content already?
-            $newExtensionAlready = DB::table('extensions')->where('extension_content', '=', $extension_content)->get();
-            // dd($newExtensionAlready);
-            if (count($newExtensionAlready) > 0) {
+        if (count($checkNewEmailAlready) <= 0) {
+            //get Email updating
+            $getEmailUpdate = DB::table('mails')
+                ->join('extensions', 'extensions.extension_id', '=', 'mails.extension_id')
+                ->where([
+                    ['mails.mail_id', '=', $id],
+                ])->get();
+            if ($getEmailUpdate[0]->extension_content != $extension_content) {
+                //check new extension_content already?
+                $newExtensionAlready = DB::table('extensions')->where('extension_content', '=', $extension_content)->get();
+                if (count($newExtensionAlready) > 0) {
+                    DB::table('mails')
+                        ->where('mail_id', $id)
+                        ->update(['extension_id' => $newExtensionAlready[0]->extension_id]);
+                } else {
+                    DB::table('extensions')
+                        ->where('extension_id', $getEmailUpdate[0]->extension_id)
+                        ->update(['extension_content' => $extension_content]);
+                }
+            }
+            if ($getEmailUpdate[0]->mail != $mail) {
                 DB::table('mails')
                     ->where('mail_id', $id)
-                    ->update(['extension_id' => $newExtensionAlready[0]->extension_id]);
+                    ->update(['mail' => $mail]);
             }
+            $request->session()->flash('msg', 'Edit Email success!');
+            return redirect()->route('home.index');
+        } else {
+            $request->session()->flash('msg', 'Email Already!');
+            return redirect()->route('home.index');
         }
     }
 
