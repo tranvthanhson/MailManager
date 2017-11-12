71fed102b77e5a6c93f6ccbc77a84db65acad311
Le Huu Tien (Lab 1 - PHP) <lehuutien1996@gmail.com>
2017-11-12 15:41:26 +0700

    Update DB Config

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-HEROKU_APP=
+HEROKU_APP=mail-manager
 
 DB_CONNECTION=pgsql
-DB_HOST=
-DB_PORT=
-DB_DATABASE=
-DB_USERNAME=
-DB_PASSWORD=
+DB_HOST=ec2-23-21-85-76.compute-1.amazonaws.com
+DB_PORT=5432
+DB_DATABASE=dcgubva22llt2m
+DB_USERNAME=krruncjhwqphpk
+DB_PASSWORD=283b30e371a4bb08432511f05047f0afdc28b3d96d6b7314f60208a38fd2b9aa
 
 heroku config:set DB_CONNECTION=$DB_CONNECTION --app=$HEROKU_APP
 heroku config:set DB_HOST=$DB_HOST --app=$HEROKU_APP
